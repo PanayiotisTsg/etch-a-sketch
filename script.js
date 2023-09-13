f440c4ced59ad8ee8d495713b01d966e5438551d
Panayiotis Tsangarides <139330855+PanayiotisTsg@users.noreply.github.com>
2023-09-13 18:58:46 +0300

    Add grid size slider
    Removed prompt input for the size
    Added slider input for the grid size
    Reduced the maximum size to 64 x 64

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const gridContainer = document.querySelector('.grid-container');
-const gridButton = document.querySelector('.grid-selection');
 let numberOfSquares = 0;
 const gridLinesButton = document.querySelector('.grid-lines-button');
 let gridLines = false; // Grid lines are not visible by default
@@ -7,56 +6,8 @@ const rainbowButton = document.querySelector('.rainbow');
 let rainbowColor = false;
 let blackColor = true;
 const blackButton = document.querySelector('.black');
-
-// Display the grid size based on input
-function getGrid() {
-    numberOfSquares = +prompt('Enter the number of squares per side: ');
-
-    // Only numbers from 0 - 100 can be accepted
-    if (numberOfSquares > 100) {
-        alert('The maximum number is 100');
-        return;
-    } else if (numberOfSquares < 0) {
-        alert('The minimum number is 0');
-        return;
-    } else if (isNaN(numberOfSquares)) {
-        alert('Only numbers can be accepted');
-        return;
-    }
-
-    // Remove every square before displaying the new size
-    while (gridContainer.hasChildNodes()) {
-        gridContainer.removeChild(gridContainer.firstChild);
-    };
-
-    // Create the squares and append them inside the container
-    for (let i = 0; i < numberOfSquares; i++) {
-        for (let j = 0; j < numberOfSquares; j++) {
-            const gridSquare = document.createElement('div');
-
-            // Divide the container width(600px) by the grid size
-            // entered by the user to get the square width
-            // Example: user input: 16 -> 600 / 16 = 37.5px
-            gridSquare.style.width = `${600 / numberOfSquares}px`;
-            gridSquare.style.height = `${600 / numberOfSquares}px`;
-            if (gridLines === true) {
-                gridSquare.style.border = '1px solid grey';
-            }
-            gridContainer.appendChild(gridSquare);
-
-            gridSquare.addEventListener('mouseover', e => {
-                if (blackColor === true){
-                    e.target.style.backgroundColor = 'black';
-                } else {
-                    e.target.style.backgroundColor = 
-                        `rgb(${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()})`;
-                }
-            });
-        }
-    }
-}
-
-gridButton.addEventListener('click', getGrid);
+const slider = document.querySelector('.slider');
+const size = document.querySelector('.size');
 
 // Toggle the grid lines to be visible or invisible
 gridLinesButton.addEventListener('click', e => {
@@ -65,16 +16,16 @@ gridLinesButton.addEventListener('click', e => {
     if (gridContainer.hasChildNodes) {
         children.forEach(square => {
             switch (square.style.border) {
-                case '1px solid grey':
+                case '1px solid var(--grid-lines)':
                     square.style.border = 'none';
                     gridLines = false;
                     break;
                 case 'none':
-                    square.style.border = '1px solid grey';
+                    square.style.border = '1px solid var(--grid-lines)';
                     gridLines = true;
                     break;
                 default:
-                    square.style.border = '1px solid grey';
+                    square.style.border = '1px solid var(--grid-lines)';
                     gridLines = true;
                     break;
             }
@@ -124,3 +75,40 @@ function toggleBlack(e) {
 
 rainbowButton.addEventListener('click', toggleRainbow);
 blackButton.addEventListener('click', toggleBlack);
+
+slider.addEventListener('input', e => {
+    size.textContent = `${e.target.value} x ${e.target.value}`
+
+    numberOfSquares = +e.target.value;
+
+    // Remove every square before displaying the new size
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    };
+
+    // Create the squares and append them inside the container
+    for (let i = 0; i < numberOfSquares; i++) {
+        for (let j = 0; j < numberOfSquares; j++) {
+            const gridSquare = document.createElement('div');
+
+            // Divide the container width(600px) by the grid size
+            // entered by the user to get the square width
+            // Example: user input: 16 -> 600 / 16 = 37.5px
+            gridSquare.style.width = `${600 / numberOfSquares}px`;
+            gridSquare.style.height = `${600 / numberOfSquares}px`;
+            if (gridLines === true) {
+                gridSquare.style.border = '1px solid var(--grid-lines)';
+            }
+            gridContainer.appendChild(gridSquare);
+
+            gridSquare.addEventListener('mouseover', e => {
+                if (blackColor === true){
+                    e.target.style.backgroundColor = 'black';
+                } else {
+                    e.target.style.backgroundColor = 
+                        `rgb(${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()})`;
+                }
+            });
+        }
+    }
+})
