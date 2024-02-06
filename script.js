c7e2d7789ad98e6fa69dfe199d3f3b653183607a
Panayiotis Tsangarides <139330855+PanayiotisTsg@users.noreply.github.com>
2024-02-06 10:30:15 +0200

    Change draw functionality from hover to hold
    1. Users are now able to draw more accurate with click and hold
    2. Changed grid lines button toggle color to be user friendly
    3. Added several functions for cleaner code
    4. Revision and removal of unnecessary code

--- script.js
+++ script.js
@@ -1,91 +1,36 @@
-const gridContainer = document.querySelector('.grid-container');
-let numberOfSquares = 0;
-const gridLinesButton = document.querySelector('.grid-lines-button');
-let gridLines = false; // Grid lines are not visible by default
-const rainbowButton = document.querySelector('.rainbow');
-let rainbowColor = false;
-let blackColor = true;
-const blackButton = document.querySelector('.black');
-const slider = document.querySelector('.slider');
-const size = document.querySelector('.size');
-
-// Toggle the grid lines to be visible or invisible
-gridLinesButton.addEventListener('click', e => {
-    const children = gridContainer.childNodes;
-
-    if (gridContainer.hasChildNodes) {
-        children.forEach(square => {
-            switch (square.style.border) {
-                case '1px solid var(--grid-lines)':
-                    square.style.border = 'none';
-                    gridLines = false;
-                    break;
-                case 'none':
-                    square.style.border = '1px solid var(--grid-lines)';
-                    gridLines = true;
-                    break;
-                default:
-                    square.style.border = '1px solid var(--grid-lines)';
-                    gridLines = true;
-                    break;
-            }
-        });
-    }
-});
-
 // Get random numbers in order to apply random colors
 function getRandomRGB() {
     return Math.floor(Math.random() * 256);
 }
 
+function getRandomColor() {
+    return `rgb(${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()})`;
+}
 
 function toggleRainbow(e) {
-    const gridSquares = gridContainer.childNodes;
-
-    // Adjust the color of the buttons based on selection
     blackColor = false;
     rainbowColor = true;
+    // Adjust the color of the buttons based on selection
     e.target.style.cssText = 'background-color: var(--primary-dark); color: var(--primary-light);';
     blackButton.style.cssText = 'background-color: var(--primary-light); color: var(--primary-dark);';
-
-    if (gridContainer.hasChildNodes) {
-        gridSquares.forEach(square => {
-            square.addEventListener('mouseover', e => {
-                e.target.style.backgroundColor = 
-                    `rgb(${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()})`;
-            })
-        })
-    }
 }
 
 function toggleBlack(e) {
-    const gridSquares = gridContainer.childNodes;
-
     blackColor = true;
     rainbowColor = false;
+    // Adjust the color of the buttons based on selection
     e.target.style.cssText = 'background-color: var(--primary-dark); color: var(--primary-light);';
     rainbowButton.style.cssText = 'background-color: var(--primary-light); color: var(--primary-dark);';
-
-    gridSquares.forEach(square => {
-        square.addEventListener('mouseover', e => {
-            e.target.style.backgroundColor = 'black';
-        })
-    })
 }
 
-rainbowButton.addEventListener('click', toggleRainbow);
-blackButton.addEventListener('click', toggleBlack);
-
-slider.addEventListener('input', e => {
+function setUpGrid(e) {
     size.textContent = `${e.target.value} x ${e.target.value}`
 
     numberOfSquares = +e.target.value;
 
     // Remove every square before displaying the new size
-    while (gridContainer.hasChildNodes()) {
-        gridContainer.removeChild(gridContainer.firstChild);
-    };
-
+    gridContainer.textContent = '';
+    
     // Create the squares and append them inside the container
     for (let i = 0; i < numberOfSquares; i++) {
         for (let j = 0; j < numberOfSquares; j++) {
@@ -96,19 +41,67 @@ slider.addEventListener('input', e => {
             // Example: user input: 16 -> 600 / 16 = 37.5px
             gridSquare.style.width = `${600 / numberOfSquares}px`;
             gridSquare.style.height = `${600 / numberOfSquares}px`;
-            if (gridLines === true) {
+            if (gridLines) {
                 gridSquare.style.border = '1px solid var(--grid-lines)';
             }
             gridContainer.appendChild(gridSquare);
-
-            gridSquare.addEventListener('mouseover', e => {
-                if (blackColor === true){
-                    e.target.style.backgroundColor = 'black';
-                } else {
-                    e.target.style.backgroundColor = 
-                        `rgb(${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()})`;
-                }
-            });
+            gridSquare.addEventListener('mousedown', setMouseHold);
         }
     }
-})
+}
+
+function setMouseHold(e) {
+    mouseDown = true;
+    setBgColor(e);
+    e.preventDefault();
+    // In case the user releases the mouse button
+    // outside of the grid container
+    document.body.addEventListener('mouseup', () => {
+        mouseDown = false;
+    })
+    // Add/remove the following event listeners
+    // in order to achieve mouse hold draw
+    const squares = gridContainer.childNodes;
+    squares.forEach(square => {
+        square.addEventListener('mouseenter', setBgColor);
+        square.addEventListener('mouseup', () => {
+            mouseDown = false;
+            squares.forEach(square => {
+                square.removeEventListener('mouseenter', setBgColor);
+            })
+        });
+    });
+}
+
+function setBgColor(e) {
+    if (mouseDown) e.target.style.backgroundColor = (blackColor) ? 'black' : getRandomColor();
+}
+
+function toggleGridLines(e) {
+    const squares = gridContainer.childNodes;
+    squares.forEach(square => {
+        square.style.border = (gridLines) ? 'none' : '1px solid var(--grid-lines)';
+    })
+    gridLines = !gridLines;
+    e.target.classList.toggle('black');
+}
+
+const gridContainer = document.querySelector('.grid-container');
+const gridLinesButton = document.querySelector('.grid-lines-button');
+const rainbowButton = document.querySelector('.rainbow');
+const blackButton = document.querySelector('.black');
+const slider = document.querySelector('.slider');
+const size = document.querySelector('.size');
+let numberOfSquares = 0;
+let gridLines = false;
+let rainbowColor = false;
+let blackColor = true;
+let mouseDown = false;
+let inputEvent = new Event('input');
+
+gridLinesButton.addEventListener('click', toggleGridLines);
+rainbowButton.addEventListener('click', toggleRainbow);
+blackButton.addEventListener('click', toggleBlack);
+slider.addEventListener('input', setUpGrid);
+// Trigger the event for the default grid
+slider.dispatchEvent(inputEvent);
